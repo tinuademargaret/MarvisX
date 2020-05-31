84f66f0160266d288a41e5e3a1466a3c28336e89
tinumide <summittinuade@gmail.com>
2020-05-31 23:54:34 +0100

    fixes

--- src/app/controllers/controller.py
+++ src/app/controllers/controller.py
@@ -1,33 +1,21 @@
 from src.app import app, socketio, emit
-# from flask import jsonify
 import requests
 import ast
-# from src.app.services.consumer import consumer
-
 
 
 class Controller:
-
     def get_bible(self, passage):
         print('I got here 5')
         response = requests.get(
             "https://bible-api.com/{passage}".format(passage=passage))
         print(response)
         return response.json()
-        # f = open('/home/tinuade/PycharmProject/project_marvis/app/templates/bible.html', "w+")
-        # f.write(response.text)
-        # f.close()
 
     def search(self, query):
         pass
 
     def run(self, message):
         print("Received message: {}".format(message))
-        # Messages.append(message.data)
-        # message.ack()
-        # message = message.data
-        # message = ast.literal_eval(message.decode('utf-8'))
-        # message = ast.literal_eval(message)
         if message.get('action') == "openBible":
             data = self.get_bible(message.get('query'))
             print("data retrieved: ", data)
--- src/app/routes/route.py
+++ src/app/routes/route.py
@@ -1,6 +1,5 @@
 from src.app.controllers.controller import *
 from flask import render_template, request, jsonify
-from src.app.services.consumer import consumer_listener
 controller_ins = Controller()
 
 
@@ -8,17 +7,16 @@ controller_ins = Controller()
 def index():
     return render_template("index.html")
 
+
 @app.route("/callback", methods=['POST'])
 def notification():
     msg = request.get_json()
-    print(msg)
-    print(type(msg))
     if not msg:
         return "Invalid data", 400
     print("processing message: ", msg)
     controller_ins.run(msg)
-    # return jsonify({"status": "Success", "code": 200})
     return "success", 200
+
 
 @socketio.on('connect', namespace='/marvis')
 def test_connect():
